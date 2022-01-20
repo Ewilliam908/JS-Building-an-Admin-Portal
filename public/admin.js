906bc3f09952ebbd77cb058b7620a24746657b8f
Edwin William <edwinwilliam908@gmail.com>
2022-01-19 23:07:33 -0500

    section 4 needs to be done

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,14 @@
 
 // Your Code Here
+let response = await fetch('http://localhost:3001/updateBook')
+    method: ,"PATCH",
+    headers: {
+    'Content-Type': 'application/json'
+},
+body: JSON.stringify({
+  "id": 3,
+  "title": "Legends of Arathrae",  
+});
+
+let updateBook = await response.json();
+console.log("Legends of Arathrae")
